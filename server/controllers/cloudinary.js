1f92fac8d0d6fd8617312c5e717af0d20f89273a
Umesh <umeshmane575@gmail.com>
2023-12-05 09:09:03 +0530

    first commit

---
+++ server/controllers/cloudinary.js
@@ -0,0 +1,67 @@
+// const cloudinary = require("cloudinary");
+
+// // config
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// // req.files.file.path
+// exports.upload = async (req, res) => {
+//   let result = await cloudinary.uploader.upload(req.body.image, {
+//     public_id: `${Date.now()}`,
+//     resource_type: "auto", // jpeg, png
+//   });
+//   res.json({
+//     public_id: result.public_id,
+//     url: result.secure_url,
+//   });
+// };
+
+// exports.remove = (req, res) => {
+//   let image_id = req.body.public_id;
+
+//   cloudinary.uploader.destroy(image_id, (err, result) => {
+//     if (err) return res.json({ success: false, err });
+//     res.send("ok");
+//   });
+// };
+
+const cloudinary = require("cloudinary");
+
+// config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+exports.upload = async (req, res) => {
+  try {
+    let result = await cloudinary.uploader.upload(req.body.image, {
+      public_id: `${Date.now()}`,
+      resource_type: "auto", // jpeg, png
+    });
+    res.json({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.remove = (req, res) => {
+  let image_id = req.body.public_id;
+
+  cloudinary.uploader.destroy(image_id, (err, result) => {
+    if (err) {
+      console.error("Error removing image:", err);
+      return res.status(500).json({ success: false, error: err });
+    }
+    res.send("ok");
+  });
+};
+
